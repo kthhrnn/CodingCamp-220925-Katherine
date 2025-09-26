08374af790a110b127df1ffd04df4b61570d7cc1
kthhrnn <katherine.ng8888@gmail.com>
2025-09-26 21:11:58 +0700

    add header, main section, and form validation to portofolio website

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// welcomeMessage();
+
+function welcomeMessage() {
+    let username = prompt("Enter your name:");
+    if (username) {
+        document.getElementById("username").textContent = username;
+        alert(`Welcome to my portofolio, ${username}!`);
+    } else {
+        alert("Welcome to my portofolio!");
+    }
+}
+
+// Form Validation
+function validateForm() {
+    // Get form values
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    // Simple validation
+    if (name === "" || email === "" || message === "") {
+        // If any field is empty,show an alert
+        alert("Please fill in all fields.");
+    } else {
+        // Update tulisan di home
+        document.getElementById("username").textContent = name;
+        
+        // If all fields are filled, show a succes message
+        alert(`Thanks, ${name}! Form submitted succesfully`);
+    }
+}
